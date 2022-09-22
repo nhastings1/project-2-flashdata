ce2ec30069566239da699ecc8a892d2aa585df4d
Jordan <jordan.b3360@gmail.com>
2022-09-22 18:56:24 -0500

    added homepage in handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,24 +1,27 @@
-const router = require('express').Router();
-// const { User } = require('../models');
+const router = require("express").Router();
+const { User } = require("../models");
 // const withAuth = require('../utils/auth');
 
-router.get('/', async (_req, res) => {
+router.get("/", async (req, res) => {
   try {
     // Change this to where you app should go
-    res.render('login');
+    // console.log(req.session.logged_in);
+    res.render("homepage", {
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     // CHANGE THIS TO WHEREVER YOUR PROJECT NEEDS TO GO
-    res.redirect('/logged_in_hompage');
+    res.redirect("/");
     return;
   }
-  res.render('login');
+  res.render("login");
 });
 
 module.exports = router;
