8fc9eb61acbb7d4381c55bb119b4e6eb1722b47f
Jordan <jordan.b3360@gmail.com>
2022-09-22 19:38:26 -0500

    added dashboard home route and create new flashcard route and handlebars pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,12 @@
-const router = require("express").Router();
-const { User } = require("../models");
-// const withAuth = require('../utils/auth');
+const router = require('express').Router();
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     // Change this to where you app should go
     // console.log(req.session.logged_in);
-    res.render("homepage", {
+    res.render('homepage', {
       logged_in: req.session.logged_in,
     });
   } catch (err) {
@@ -14,14 +14,26 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/login", (req, res) => {
+router.get('/dashboard', withAuth, (req, res) => {
+  res.render('dashboard', {
+    logged_in: req.session.logged_in,
+  });
+});
+
+router.get('/new-card', withAuth, (req, res) => {
+  res.render('new-card', {
+    logged_in: req.session.logged_in,
+  });
+});
+
+router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     // CHANGE THIS TO WHEREVER YOUR PROJECT NEEDS TO GO
-    res.redirect("/");
+    res.redirect('/dashboard');
     return;
   }
-  res.render("login");
+  res.render('login');
 });
 
 module.exports = router;
--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -2,23 +2,23 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
-  const email = document.querySelector("#email-login").value.trim();
-  const password = document.querySelector("#password-login").value.trim();
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
     // Send a POST request to the API endpoint
-    const response = await fetch("/api/users/login", {
-      method: "POST",
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
       body: JSON.stringify({ email, password }),
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace("/");
+      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
-      console.log("test");
+      console.log('test');
     }
   }
 };
@@ -26,19 +26,19 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector("#name-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
 
   if (name && email && password) {
-    const response = await fetch("/api/users", {
-      method: "POST",
+    const response = await fetch('/api/users', {
+      method: 'POST',
       body: JSON.stringify({ name, email, password }),
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
@@ -46,9 +46,9 @@ const signupFormHandler = async (event) => {
 };
 
 document
-  .querySelector(".login-form")
-  .addEventListener("submit", loginFormHandler);
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
 
 document
-  .querySelector(".signup-form")
-  .addEventListener("submit", signupFormHandler);
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
