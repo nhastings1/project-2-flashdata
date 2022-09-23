b62f9e32b4fa6e28a8db467121f5e0d5693ef19e
Brad <bycarlson23@gmail.com>
2022-09-22 19:57:06 -0500

    set up index.js in models folder

--- models/index.js
+++ models/index.js
@@ -1,3 +1,35 @@
 const User = require('./User');
+const Card = require('./Card');
+const Collection = require('./Collection');
 
-module.exports = { User };
+User.hasMany(Card, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Collection, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});
+
+Card.belongsTo(User, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});
+
+Card.hasOne(Collection, {
+    foreignKey: 'collectionId',
+    onDelete: 'CASCADE'
+});
+
+Collection.belongsTo(User, {
+    foreignKey: 'userId',
+    onDelete: 'CASCASE'
+});
+
+Collection.hasMany(Card, {
+    foreignKey: 'collecitonId',
+    onDelete: 'CASCADE'
+});
+
+module.exports = { User, Card, Collection };
