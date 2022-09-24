52219b822ba4f3029e84fb2f2b10354bab3c9692
Ryan <thisianemailaddressthatis@gmail.com>
2022-09-24 08:56:26 -0500

    fixed route to force login

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
   try {
     // Change this to where you app should go
     // console.log(req.session.logged_in);
