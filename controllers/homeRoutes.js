e144c13495e3dc54db197a34fc54cd64ec1fbafa
Jordan <jordan.b3360@gmail.com>
2022-09-27 18:55:55 -0500

    update

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,17 +1,19 @@
 const router = require('express').Router();
-const { User, Card, Collection } = require('../models');
+const { Card, Collection } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
+  console.log();
   try {
     // Change this to where you app should go
     // console.log(req.session.logged_in);
     const cardData = await Card.findAll({
       order: [['id', 'ASC']],
+      // where: { id: req.id },
     });
     const cards = cardData.map((project) => project.get({ plain: true }));
     console.log('cards:', cards);
-    res.render('homepage', {
+    res.render('flashcards', {
       cards,
       logged_in: req.session.logged_in,
     });
@@ -40,25 +42,40 @@ router.get('/dashboard', withAuth, async (req, res) => {
 });
 
 router.get('/cards/:id', withAuth, async (req, res) => {
-  console.log('i am here in cards');
   try {
-    // console.log('***req***', req.params.id);
-    // const cardData = await Card.findAll({
-    //   order: [['id', 'ASC']],
-    // });
-    // const cards = cardData.map((project) => project.get({ plain: true }));
-    // console.log('cards:', cards);
-    // res.render('homepage', {
-    //   cards,
-    //   logged_in: req.session.logged_in,
-    // });
+    const cardData = await Card.findAll({
+      order: [['id', 'ASC']],
+      where: { collection_id: req.params.id },
+    });
+    const cards = cardData.map((project) => project.get({ plain: true }));
+    res.render('flashcards', {
+      cards,
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/new-card', withAuth, (req, res) => {
-  res.render('new-card', {
+router.get('/new-card', withAuth, async (req, res) => {
+  try {
+    const collectionsData = await Collection.findAll({
+      order: [['name', 'ASC']],
+    });
+    const collections = collectionsData.map((project) =>
+      project.get({ plain: true })
+    );
+    res.render('new-card', {
+      collections,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/new-collection', withAuth, (req, res) => {
+  res.render('new-collection', {
     logged_in: req.session.logged_in,
   });
 });
