6617e7d7bde221be48900efbfa7bcbf9c2b7c236
Brad <bycarlson23@gmail.com>
2022-09-26 23:14:32 -0500

    trying to add delete route to router

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -72,4 +72,24 @@ router.get('/login', (req, res) => {
   }
   res.render('login');
 });
+
+// router.delete('/:id', withAuth, async (req, res) => {
+//   try {
+//     const deleteCard = await Card.destroy({
+//       where: {
+//         id: req.params.id,
+//         user_id: req.session.user_id,
+//       },
+//     });
+
+//     if (!deleteCard) {
+//       res.status(404).json({ message: 'No project found with this id!' });
+//       return;
+//     }
+
+//     res.status(200).json(deleteCard);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 module.exports = router;
