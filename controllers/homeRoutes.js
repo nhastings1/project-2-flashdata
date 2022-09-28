413a88b879cbf25e120898b9d56c479dd78b55f5
Ryan <thisianemailaddressthatis@gmail.com>
2022-09-28 13:25:24 -0500

    ui changes and reveal button

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,24 +2,14 @@ const router = require('express').Router();
 const { Card, Collection } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
-  console.log();
-  try {
-    // Change this to where you app should go
-    // console.log(req.session.logged_in);
-    const cardData = await Card.findAll({
-      order: [['id', 'ASC']],
-      // where: { id: req.id },
-    });
-    const cards = cardData.map((project) => project.get({ plain: true }));
-    console.log('cards:', cards);
-    res.render('flashcards', {
-      cards,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
+router.get('/', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    // CHANGE THIS TO WHEREVER YOUR PROJECT NEEDS TO GO
+    res.redirect('/dashboard');
+    return;
   }
+  res.render('login');
 });
 
 router.get('/dashboard', withAuth, async (req, res) => {
--- public/assets/js/showHide.js
+++ public/assets/js/showHide.js
@@ -1,16 +1,22 @@
-function showHide() {
-  // console.log('test');
-  for (let i = 0; i < toggleBtn.length; i++) {
-    const showHide = document.getElementsByClassName('show-hide')[i];
-
-    if (showHide.style.display === 'none') {
-      showHide.style.display = 'block';
-    } else {
-      showHide.style.display = 'none';
-    }
+function showHide(id) {
+  console.log('test', id);
+  const el = document.getElementById(id);
+  if (el.style.visibility === 'hidden') {
+    el.style.visibility = 'visible';
+  } else {
+    el.style.visibility = 'hidden';
   }
+  // for (let i = 0; i < toggleBtn.length; i++) {
+  //   const showHide = document.getElementsByClassName('show-hide')[i];
+
+  //   if (showHide.style.display === 'none') {
+  //     showHide.style.display = 'block';
+  //   } else {
+  //     showHide.style.display = 'none';
+  //   }
+  // }
 }
-var toggleBtn = document.querySelectorAll('.toggle-btn');
-for (let i = 0; i < toggleBtn.length; i++) {
-  toggleBtn[i].addEventListener('click', showHide);
-}
+// var toggleBtn = document.querySelectorAll('.toggle-btn');
+// for (let i = 0; i < toggleBtn.length; i++) {
+//   toggleBtn[i].addEventListener('click', showHide);
+// }
